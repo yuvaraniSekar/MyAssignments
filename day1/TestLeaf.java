6dc2bb9ecf94f95e3d1a90b0295a18b7b3ce2ce6
yuvaraniSekar <ysyuvaranis@gmail.com>
2025-01-06 12:11:38 +0530

    Add files via upload
    Week 1

---
+++ day1/TestLeaf.java
@@ -0,0 +1,10 @@
+package week1.day1;
+
+public class TestLeaf {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Test leaf");
+
+	}
+
+}
---
+++ day2/Firefox.java
@@ -0,0 +1,14 @@
+package week1.day2;
+
+public class Firefox {
+	public static void main(String[] args) {
+		float browserVersion = 100.2f;
+		String browserName = "firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo ='f';
+		System.out.println("Browser version is: " + browserVersion + " " + "Browser name is: " + browserName);
+		System.out.println("browser is visible :" + isVisible + "Release year :" + releaseYear + " " + "Browser Logo :" + browserLogo);
+	}
+
+}
